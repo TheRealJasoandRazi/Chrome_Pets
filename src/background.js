ab8d2266dfe1b4be4bd535a2328a8d1eccdc2925
TheRealRaziandJaso <Neno701@outlook.com>
2024-02-21 14:44:01 +1100

    worker checks for data stored in local storage

--- src/background.js
+++ src/background.js
@@ -14,5 +14,16 @@ function send_position() {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       chrome.tabs.sendMessage(tabs[0].id, { message: "myMessage" });
     });
+    chrome.storage.local.get(["Pet_Data"]).then((result) => {
+        console.log(result);
+        console.log(result.Pet_Data);
+        if(result.Pet_Data !== undefined){
+            console.log("got data");
+        } else {
+            chrome.storage.local.set({ Pet_Data: "stuff in here" }).then(() => {
+                console.log("Value is set");
+            });
+        }
+    });
   }
 chrome.tabs.onActivated.addListener(send_position);
