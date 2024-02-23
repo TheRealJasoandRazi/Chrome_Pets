dba7971354c59263341d81196618ebe85fcede3b
TheRealRaziandJaso <Neno701@outlook.com>
2024-02-23 16:03:04 +1100

    new pets created, position passed around

--- src/Pet.js
+++ src/Pet.js
@@ -1,26 +1,43 @@
-const My_Pet = document.createElement('div');
-My_Pet.innerHTML = "Pet.png";
-My_Pet.style.width = "100px";
-My_Pet.style.height = "100px";
-My_Pet.style.backgroundColor = "Blue";
-My_Pet.style.position = 'fixed';
-My_Pet.style.top = '50%';
-My_Pet.style.left = '50%';
+let My_Pet;
 
-document.body.appendChild(My_Pet);
-My_Pet.addEventListener("click", Pet);
-My_Pet_Move();
-
-/*
-chrome.runtime.onMessage.addListener(CreatePet);
-function CreatePet(message){
-  console.log(message);
-}*/
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  if (message.message === "myMessage") {
-    console.log("recieved message");
+  if (message.message === "return_position") {
+    if (My_Pet && My_Pet.style && My_Pet.style.left !== null) {
+      chrome.runtime.sendMessage({
+        Pet_Data_X: My_Pet.style.left,
+        Pet_Data_Y: My_Pet.style.top
+      })
+      My_Pet.remove();
+    } else {
+      chrome.runtime.sendMessage({
+        Pet_Data_X: window.innerWidth / 2 + "px",
+        Pet_Data_Y: window.innerHeight / 2 + "px"
+      })
+    }
+  } else if (message.message === "New_Pet") {
+      Create_Pet(message.Pet_Data_Y, message.Pet_Data_X);
   }
 });
+
+function Create_Pet(top, left){
+  const newPet = document.createElement('div');
+  newPet.innerHTML = "Pet.png";
+  newPet.style.width = "100px";
+  newPet.style.height = "100px";
+  newPet.style.backgroundColor = "Blue";
+  newPet.style.position = 'fixed';
+  newPet.style.top = top;
+  newPet.style.left = left;
+
+  document.body.appendChild(newPet);
+
+  My_Pet = newPet;
+
+  My_Pet.addEventListener("click", Pet);
+  My_Pet.addEventListener('transitionend', Time_Out);
+
+  My_Pet_Move();
+}
 
 function My_Pet_Move() {
   const randomAngle = Math.random() * 360;
@@ -29,20 +46,30 @@ function My_Pet_Move() {
   const translateX = distance * Math.cos(angleInRadians);
   const translateY = distance * Math.sin(angleInRadians);
 
-  requestAnimationFrame(() => {
+  //update position of pet, this doesn't move the pet but is used when it returns its position
+  newTop = (parseFloat(My_Pet.style.top) + translateY) + "px";
+  newLeft = (parseFloat(My_Pet.style.left) + translateX) + "px";
+  
+  const windowHeight = window.innerHeight;
+  const windowWidth = window.innerWidth;
+
+  //makes sure the pet can't move outside of the window screen
+  My_Pet.style.top = Math.max(0, Math.min(newTop, windowHeight - My_Pet.style.height)) + "px";
+  My_Pet.style.left = Math.max(0, Math.min(newLeft, windowWidth - My_Pet.style.width)) + "px";
+
+  requestAnimationFrame(() => { //this code waits for the next available frame and moves the div
     My_Pet.style.transition = '5s';
     My_Pet.style.transform = `translate(${translateX}px, ${translateY}px)`;
   });
-  console.log("moved")
+
 }
 
-function Time_Out() {
+function Time_Out() { //creates a delay before moving the pet again
   setTimeout(My_Pet_Move, 2000);
 }
 
-function Pet() {
+function Pet() { //the pet action
   let randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
   My_Pet.style.backgroundColor = randomColor;
 }
 
-My_Pet.addEventListener('transitionend', Time_Out);
--- src/background.js
+++ src/background.js
@@ -1,29 +1,29 @@
-/*const default_pet_position = {
-    x: Math.floor(window.innerWidth - (0.1 * window.innerWidth)),
-    y: Math.floor(window.innerHeight / 2)
-};
-*/ //need to use chrome.windows api for this
-//temp solution
-
-const default_pet_position = {
-    x: 100,
-    y: 100
-}
-
-function send_position() {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { message: "myMessage" });
-    });
-    chrome.storage.local.get(["Pet_Data"]).then((result) => {
-        console.log(result);
-        console.log(result.Pet_Data);
-        if(result.Pet_Data !== undefined){
-            console.log("got data");
-        } else {
-            chrome.storage.local.set({ Pet_Data: "stuff in here" }).then(() => {
-                console.log("Value is set");
-            });
-        }
+function send_position() { //sendds position to the current tab
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, { message: "return_position" });
     });
   }
-chrome.tabs.onActivated.addListener(send_position);
+chrome.tabs.onActivated.addListener(send_position); //recieves position from old tab
+
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    console.log(message.Pet_Data_X);
+    console.log(message.Pet_Data_Y);
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, 
+        { 
+            message: "New_Pet", Pet_Data_X: message.Pet_Data_X, Pet_Data_Y: message.Pet_Data_Y 
+        });
+    });
+});
+
+/*
+chrome.storage.local.get(["Pet_Data"]).then((result) => {
+    if(result.Pet_Data !== undefined){
+        //console.log("got data");
+    } else {
+        chrome.storage.local.set({ Pet_Data: "stuff in here" }).then(() => {
+            //console.log("Value is set");
+        });
+    }
+});
+*/ //dont even need storage for it?
