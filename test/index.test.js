7f5e314bc57d83482a845a4d26f1d553c57b91a1
TheRealRaziandJaso <Neno701@outlook.com>
2024-03-22 12:05:37 +1100

    chenged pupeteer properties

--- test/index.test.js
+++ test/index.test.js
@@ -13,8 +13,8 @@ beforeEach(async () => { //jest code helps structure the tests with puppeteer
     args: [
       `--disable-extensions-except=${EXTENSION_PATH}`,
       `--load-extension=${EXTENSION_PATH}`,
-      //`--no-sandbox`,
-      //'--disable-setuid-sandbox',
+      `--no-sandbox`,
+      '--disable-setuid-sandbox',
       '--disable-web-security',
       '--allow-running-insecure-content',
       '--disable-features=IsolateOrigins',
